e94ac383139ac86aa7bf94764b39d896a651f56c
Anton Levashov <antn.lvshv@gmail.com>
2023-11-27 08:03:13 +0000

    Add lxml library for web scraping

--- digital_analyst/main.py
+++ digital_analyst/main.py
@@ -1,7 +1,8 @@
-from digital_analyst.scraper import parse_sitemap, extract_text
+from scraper import parse_sitemap, extract_text
 
 def main():
-    sitemap_url = input("Enter the sitemap URL: ")
+    # sitemap_url = input("Enter the sitemap URL: ")
+    sitemap_url = "https://www.kaushik.net/sitemap.xml"
     urls = parse_sitemap(sitemap_url)
     for url in urls:
         text = extract_text(url)
